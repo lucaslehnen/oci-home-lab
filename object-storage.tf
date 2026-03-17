4f09d5bb80b61cde9122762a9ccfdec815ef8d67
Lucas Lehnen <lucaslehnen@outlook.com>
2026-03-16 23:41:27 -0300

    chore: Bucket for backups

---
+++ object-storage.tf
@@ -0,0 +1,45 @@
+# Object Storage Bucket for Backups
+# Tier: Standard (default)
+# Lifecycle: Objects move to Archive after 1 week
+
+resource "oci_objectstorage_bucket" "backups" {
+  compartment_id = var.compartment_ocid
+  namespace      = data.oci_objectstorage_namespace.ns.namespace
+
+  name       = "backups"
+  storage_tier = "Standard"
+}
+
+resource "oci_objectstorage_object_lifecycle_policy" "backups_lifecycle" {
+  namespace = data.oci_objectstorage_namespace.ns.namespace
+  bucket    = oci_objectstorage_bucket.backups.name
+
+  rules {
+    name        = "archive_after_7_days"
+    action      = "ARCHIVE"
+    target      = "objects"
+    time_amount = 7
+    time_unit   = "DAYS"
+    is_enabled  = true
+  }
+}
+
+data "oci_objectstorage_namespace" "ns" {
+  compartment_id = var.compartment_ocid
+}
+
+# Output bucket details
+output "bucket_name" {
+  description = "Name of the backups bucket"
+  value       = oci_objectstorage_bucket.backups.name
+}
+
+output "bucket_id" {
+  description = "OCID of the backups bucket"
+  value       = oci_objectstorage_bucket.backups.id
+}
+
+output "bucket_namespace" {
+  description = "Object Storage namespace"
+  value       = data.oci_objectstorage_namespace.ns.namespace
+}
